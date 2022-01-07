3fd054db9b48941635aa8b561813e272a738ee1f
rheophile10 <rheophile10@gmail.com>
2022-01-07 04:24:29 -0700

    bad rules implementation - conways cancer resulting

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::{
     io::{stdout},
     thread::sleep,
     time::Duration,
-    iter::FromIterator,
 };
 use crossterm::{
     style::{Color, PrintStyledContent, style, 
@@ -10,27 +9,132 @@ use crossterm::{
     ExecutableCommand, Result, cursor
 };
 
-const HEIGHT:usize = 20;
-const WIDTH:usize = 20;
+//const HEIGHT:usize = 20;
+//const WIDTH:usize = 20;
 const TURNS:u8= 20;
+
+#[derive(Clone, Copy)]
+struct Environment {
+    data: [[bool; 50]; 50],
+}
+
+impl Environment {
+    pub fn new() -> Environment {
+        Environment {
+            data: [[false; 50]; 50]
+        }
+    }
+
+}
+
+fn load_live_cells(live_cells: [(usize, usize); 5], environment: &mut Environment) {
+    for cell in live_cells {
+        environment.data[cell.0][cell.1] = true;
+    }
+}
+
+fn get_live_neigbour_count(coords: (usize, usize), environment: &Environment) -> u8 {
+    let x = coords.0;
+    let y = coords.1;
+    let mut neighbour_coords = Vec::new();
+    if x == 0 {
+        neighbour_coords.push((x+1, y));
+        if y != 0 {
+            neighbour_coords.push((x+1, y-1));
+            neighbour_coords.push((x, y-1));
+        }
+        if y != 49 {
+            neighbour_coords.push((x+1, y+1));
+            neighbour_coords.push((x, y+1));
+        }             
+    } else if x == 49 {
+        neighbour_coords.push((x-1, y));
+        if y != 0 {
+            neighbour_coords.push((x-1, y-1));
+            neighbour_coords.push((x, y-1));
+        }
+        if y != 49 {
+            neighbour_coords.push((x-1, y+1));
+            neighbour_coords.push((x, y+1));
+        }       
+    } else if y == 0 {
+        neighbour_coords.push((x, y+1));
+        if x != 0 {
+            neighbour_coords.push((x-1, y+1));
+            neighbour_coords.push((x-1, y));
+        }
+        if x != 49 {
+            neighbour_coords.push((x+1, y+1));
+            neighbour_coords.push((x+1, y));
+        }    
+    } else if y == 49 {
+        neighbour_coords.push((x, y-1));
+        if x != 0 {
+            neighbour_coords.push((x-1, y-1));
+            neighbour_coords.push((x-1, y));
+        }
+        if x != 49 {
+            neighbour_coords.push((x+1, y-1));
+            neighbour_coords.push((x+1, y));
+        }  
+    } else {
+        neighbour_coords.push((x, y-1));
+        neighbour_coords.push((x-1, y-1));
+        neighbour_coords.push((x+1, y-1));
+        neighbour_coords.push((x+1, y));
+        neighbour_coords.push((x-1, y));
+        neighbour_coords.push((x, y+1));
+        neighbour_coords.push((x-1, y+1));
+        neighbour_coords.push((x+1, y+1));
+    }
+    let mut count = 0;
+    for coord in neighbour_coords {
+        if environment.data[coord.0][coord.1] {
+            count = count + 1;
+        }
+    }
+    count
+}
+
+fn row_to_string(row: usize, environment: &Environment) -> String {
+    let mut row_string = String::from("");
+    for col in 0..50 {
+        match environment.data[row][col] {
+            true => {
+                row_string.push_str("█");
+            }
+            false => {
+                row_string.push_str(" ");
+            }
+
+        }
+    }
+    row_string
+}
 
 fn main() -> Result<()> {
     let mut stdout = stdout();
-    
-    let mut environment = [[' '; HEIGHT]; WIDTH];
-    environment[0][0] = '█';
-    let mut new_environment = [[' '; HEIGHT]; WIDTH];
     stdout.execute(cursor::RestorePosition)?;
 
-    for _ in 0..=TURNS-2 {
-        for row in 0..=HEIGHT-1 {
-            for col in 0..=WIDTH-1{
-                if environment[row][col] == '█' {
-                    new_environment[row][col] = ' ';
-                    new_environment[row+1][col+1] = '█';
+    let mut environment = Environment::new();
+    let mut new_environment = Environment::new();
+    load_live_cells([(0,0),(1,0),(0,1),(1,2),(2,1)], &mut environment);
+    
+    for _ in 0..TURNS {
+        for row in 0..50 {
+            for col in 0..50 {
+                let neighbours = get_live_neigbour_count((row,col), &environment);
+                if neighbours > 2 {
+                    new_environment.data[row][col] = true;
+                } else if neighbours > 1 && environment.data[row][col] {
+                    new_environment.data[row][col] = true;
+                } else {
+                    new_environment.data[row][col] = false;
                 }
             }
-            let mut row_state = String::from_iter(new_environment[row]);
+
+
+            let mut row_state = row_to_string(row, &new_environment);
             row_state.push_str("\n");
             let stylized_row = style(row_state);
             stdout
